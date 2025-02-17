97332696582dd345c10df6b73fac68eb873c46b7
Nate <jr75988@mail.harpercollege.edu>
2025-02-17 17:02:52 -0600

    first commit

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,17 @@
 package org.example;
+import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("You can create a driver program for the converter here!");
+    System.out.println("The Ounces to Pounds Converter has began running.");
+    System.out.println("Please enter how many ounces you have");
+    Scanner obj = new Scanner(System.in);
+    int input = obj.nextInt();
+    System.out.println("Thank you. The converter is currently working to convert your input");
+    //String output = toPounds(input);
+    Converter newconverter = new Converter();
+    System.out.println(newconverter.toPounds(input));
+    System.out.println(newconverter.toPoundsAndOunces(input));
+    System.out.println("Thank you for using the Converter! Ounces to Pounds Converter is now closing.");
   }
 }
--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -2,4 +2,16 @@ package org.example;
 
 public class Converter {
   // Your code goes here
+  String toPounds(int x){
+    double y = 0.0000;
+    y = x/16.0000;
+    return y + " lbs";
+  }
+  String toPoundsAndOunces(int x){
+    int y = 0;
+    int z = 0;
+    y = x/16;
+    z = x%16;
+    return y + " lbs " + z + " oz";
+  }
 }
